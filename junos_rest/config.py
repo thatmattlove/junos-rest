3c07fed10c64ff92e8109214a91cec89571eaeb1
checktheroads <matt@allroads.io>
2020-01-27 11:55:47 -0700

    change config file location

--- junos_rest/config.py
+++ junos_rest/config.py
@@ -10,15 +10,38 @@ import yaml
 from junos_rest.models.config import Config
 
 WORKING_DIR = Path(__file__).parent
-CONFIG_FILE = WORKING_DIR / "config.yaml"
+POTENTIAL_CONFIG_LOCATIONS = [
+    Path(__file__).parent.parent,
+    Path.home(),
+    Path(__file__).parent,
+    Path("/etc/junos-rest/"),
+]
 
-if not CONFIG_FILE.exists():
-    CONFIG_FILE = WORKING_DIR / "config.yml"
+_matched_config_file = None
+for path in POTENTIAL_CONFIG_LOCATIONS:
+    file_path = path / "junos_rest.yaml"
+    if file_path.exists():
+        _matched_config_file = file_path
+        break
 
-if CONFIG_FILE.exists():
-    with CONFIG_FILE.open("r") as raw:
-        unvalidated_config = yaml.safe_load(raw.read())
-else:
-    raise Exception("No config file found.")
+if _matched_config_file is None:
+    raise Exception(
+        "No config file found. A file named 'junos_rest.yaml' must exist in one of the following paths: {p}".format(
+            p=", ".join([str(p) for p in POTENTIAL_CONFIG_LOCATIONS])
+        )
+    )
+with _matched_config_file.open("r") as raw:
+    unvalidated_config = yaml.safe_load(raw.read())
 
 params = Config(**unvalidated_config)
+
+# CONFIG_FILE = WORKING_DIR / "config.yaml"
+
+# if not CONFIG_FILE.exists():
+#    CONFIG_FILE = WORKING_DIR / "config.yml"
+
+# if CONFIG_FILE.exists():
+#     with CONFIG_FILE.open("r") as raw:
+#         unvalidated_config = yaml.safe_load(raw.read())
+# else:
+#     raise Exception("No config file found.")
